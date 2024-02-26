8bf08d2a134cb3370ffc4c3fc801ed13414a8123
Aisha Schuhegger <schuhegger@cip.ifi.lmu.de>
2024-02-26 19:04:00 +0100

    input rg complete and commented

--- scripts_hdl_ldsc/input_rg_create.R
+++ scripts_hdl_ldsc/input_rg_create.R
@@ -1,15 +1,26 @@
-# This script creates a input_rg.txt file in order to be able to use ldsc-network-plot
+# This script creates input_rg.txt files (for ldsc and hdl) in order to be able to use the ldsc-network-plot
 library(stringr)
 
+# Set working directory 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
-folder_path = "../output_ld_regression"
 
-df_adjusted <- read.table(paste(folder_path, "/complete_df_adjusted.tsv", sep=""), header=TRUE)
-df_adjusted <- subset(df_adjusted, select = -c(p1 , p2))
+# Set folder paths
+folder_path_ldsc = "../output_ld_regression"
+folder_path_hdl = "../output_hdl"
+
+# Read in the significant corrected gc values for ldsc and hdl 
+df_ldsc <- read.table(paste(folder_path_ldsc, "/df_all_adjusted_significant_ldsc.tsv", sep=""), header=TRUE)
+df_ldsc <- subset(df_ldsc, select = -c(p1 , p2))
+
+df_hdl <- read.table(paste(folder_path_hdl, "/df_all_adjusted_significant_hdll.tsv", sep=""), header=TRUE)
+df_hdl <- subset(df_hdl, select = -c(p1 , p2))
 
 # Adjust column names 
-colnames(df_adjusted)[colnames(df_adjusted) == "p1_id"] <- "p1"
-colnames(df_adjusted)[colnames(df_adjusted) == "p2_id"] <- "p2"
+colnames(df_ldsc)[colnames(df_ldsc) == "p1_id"] <- "p1"
+colnames(df_ldsc)[colnames(df_ldsc) == "p2_id"] <- "p2"
+
+colnames(df_hdl)[colnames(df_hdl) == "p1_id"] <- "p1"
+colnames(df_hdl)[colnames(df_hdl) == "p2_id"] <- "p2"
 
 trait_category_mapping <- c("IL" = "Cytokine",
                             "MS" = "Autoimmune",
@@ -42,30 +53,74 @@ trait_category_mapping <- c("IL" = "Cytokine",
                             "C3" = "Protein",
                             "HGF" = "Protein",
                             "MCP1" = "Protein",
-                            "CRP" = "Protein")
+                            "CRP" = "Protein",
+                            "APH"  = "Autoimmune",
+                            "tryptophan" = "Aminoacid",
+                            "histidine" = "Aminoacid",
+                            "phenylalanine" = "Aminoacid",
+                            "isoleucine" = "Aminoacid",
+                            "threonine" = "Aminoacid",
+                            "tyrosine" = "Aminoacid",
+                            "arginine" = "Aminoacid",
+                            "proline" = "Aminoacid",
+                            "acetylcarnitine" = "Other",
+                            "glutamate" = "Aminoacid",
+                            "alanine" = "Aminoacid",
+                            "asparagine" = "Aminoacid",
+                            "cholesterol" = "Other",
+                            "pyruvate" = "Other",
+                            "citrate" = "Other",
+                            "urea" = "Other",
+                            "glycerol" = "Other")
 
-# map the categories to the traits 
-df_adjusted$p1_helper <- df_adjusted$p1
-df_adjusted$p2_helper <- df_adjusted$p2
+# Prerequisite for mapping the categories to the traits 
+df_ldsc$p1_helper <- df_ldsc$p1
+df_ldsc$p2_helper <- df_ldsc$p2
 
-df_adjusted$p1_helper <- sapply(str_split(df_adjusted$p1_helper, "_"), function(x) x[[2]])
-df_adjusted$p2_helper <- sapply(str_split(df_adjusted$p2_helper, "_"), function(x) x[[2]])
+df_ldsc$p1_helper <- sapply(str_split(df_ldsc$p1_helper, "_"), function(x) x[[2]])
+df_ldsc$p2_helper <- sapply(str_split(df_ldsc$p2_helper, "_"), function(x) x[[2]])
+
+df_hdl$p1_helper <- df_hdl$p1
+df_hdl$p2_helper <- df_hdl$p2
+
+df_hdl$p1_helper <- sapply(str_split(df_hdl$p1_helper, "_"), function(x) x[[2]])
+df_hdl$p2_helper <- sapply(str_split(df_hdl$p2_helper, "_"), function(x) x[[2]])
 
 
 for (key in names(trait_category_mapping)) {
   
-  indices_p1 <- grepl(paste0("^", key), df_adjusted$p1_helper)
-  indices_p2 <- grepl(paste0("^", key), df_adjusted$p2_helper)
+  # Map ldsc categories and traits
+  indices_p1 <- grepl(paste0("^", key), df_ldsc$p1_helper)
+  indices_p2 <- grepl(paste0("^", key), df_ldsc$p2_helper)
   
-  df_adjusted$p1_category[indices_p1] <- trait_category_mapping[key]
-  df_adjusted$p2_category[indices_p2] <- trait_category_mapping[key]
+  df_ldsc$p1_category[indices_p1] <- trait_category_mapping[key]
+  df_ldsc$p2_category[indices_p2] <- trait_category_mapping[key]
+  
+  # Map hdl categories and traits
+  indices_p1 <- grepl(paste0("^", key), df_hdl$p1_helper)
+  indices_p2 <- grepl(paste0("^", key), df_hdl$p2_helper)
+  
+  df_hdl$p1_category[indices_p1] <- trait_category_mapping[key]
+  df_hdl$p2_category[indices_p2] <- trait_category_mapping[key]
 }
 
-df_adjusted <- subset(df_adjusted, p1 != p2)
-df_adjusted <- subset(df_adjusted, select = c("p1_category", "p1", "p2_category", "p2", "rg", "se", "z", "p", "q"))
+df_ldsc <- subset(df_ldsc, p1 != p2)
+df_ldsc <- subset(df_ldsc, select = c("p1_category", "p1", "p2_category", "p2", "rg", "se", "z", "p", "q"))
 
-df_filtered <- df_adjusted[df_adjusted$rg != 0, ] # exclude gwas-gwas pair where gc=0 (either it was >1 or <-1 or it was NA or truly totally independent --> no handling planned (also if truly independen (0) then it would just be an independent point in the plot))
+df_hdl <- subset(df_hdl, p1 != p2)
+df_hdl <- subset(df_hdl, select = c("p1_category", "p1", "p2_category", "p2", "rg", "se", "z", "p", "q"))
 
-write.table(df_filtered, file = paste(folder_path, "/input_rg_categories_filtered.txt", sep=""), sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+# Exclude GWAS pairs where gc=0 
+# -> Either it was >1 or <-1 or it was NA or truly totally independent 
+# --> no handling planned 
+# also if truly independent (0) then it would just be an independent point in the plot))
+
+
+df_ldsc <- df_ldsc[df_ldsc$rg != 0, ] 
+df_hdl <- df_hdl[df_hdl$rg != 0, ] 
+
+# Save as txt files 
+write.table(df_ldsc, file = paste(folder_path_ldsc, "/input_rg_ldsc.txt", sep=""), sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
+write.table(df_hdl, file = paste(folder_path_hdl, "/input_rg_hdl.txt", sep=""), sep = "\t", row.names = FALSE, col.names = TRUE, quote = FALSE)
 
 
